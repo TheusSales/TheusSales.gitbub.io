d980022d96f130b32e1ed89a02f118aab7276c66
Matheus Sales <61158978+TheusSales@users.noreply.github.com>
2023-08-15 11:43:16 -0300

    first commit

---
+++ poke.js
@@ -0,0 +1,57 @@
+
+var container = document.getElementById('container');
+
+async function buscar(){
+    try {
+        let pokeNameInput = document.getElementById('entra'); // Obtém o elemento input
+        let pokeName = pokeNameInput.value.toLowerCase(); // Obtém o valor do input e converte para minúsculas
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+        var data = await response.json();
+        mostrarPokemon(data,pokeName);
+        //const prettyData = JSON.stringify(data, null, 2);
+    } catch (error) {
+        window.alert(`Ocorreu um erro: ${error}`);
+    }
+}
+
+function mostrarPokemon(jsonObj){
+    var res = document.getElementById("res");
+
+    if (!res) {
+        res = document.createElement('div');
+        res.id = 'res';
+        container.appendChild(res);
+    }
+
+    res.innerHTML = '';
+
+    //imagem
+    let imageFront = jsonObj.sprites.front_default;
+    let imageBack = jsonObj.sprites.back_default;
+
+    res.innerHTML += `<img src="${imageFront}" alt="pokemon-frente">`;
+    res.innerHTML += `<img src="${imageBack}" alt="pokemon-costas">`;
+
+    //habilidades
+    res.innerHTML += `<strong><br>Habilidades<br></strong>`;
+    
+    jsonObj.abilities.forEach(habilidade => {
+        var abilityName = habilidade.ability.name;
+        res.innerHTML += `${abilityName}<br>`;
+    });
+
+    
+    //tipo do pokemon
+    res.innerHTML +=`<strong><br>Tipo:<br></strong>`
+
+    jsonObj.types.forEach(tipos => {
+        var tipo = tipos.type.name;
+        res.innerHTML += `${tipo}<br>`;
+    });
+
+    //peso
+    let peso = Math.floor(jsonObj.weight/10);
+    res.innerHTML += `<strong><br>Peso</strong>`
+    res.innerHTML += `<br>${peso} Kg<br>`;
+
+}
